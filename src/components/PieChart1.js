1abc227b620391a0559bf9d9dc8da55d46da792b
Yvan Debroux ydbweb.com <yvan.debroux@ydbweb.com>
2022-04-16 01:15:49 +0200

    Revert "update"
    This reverts commit cc085b58542449c1c8bbe9889839ba2a4a440e02.

--- src/components/PieChart1.js
+++ src/components/PieChart1.js
@@ -24,24 +24,24 @@ class PieChart1 extends Component {
       this.handlegraphBgcolor=this.handlegraphBgcolor.bind(this);
       this.handlegraphFontcolor=this.handlegraphFontcolor.bind(this);
 
-    }
+    }      
 
-    componentDidMount() {
+    componentDidMount() {  
         const tmp=this.state.topbot;
         tmp.legendType=this.props.legendType;
         this.setCanvasSize(this.props.legendType);
         this.setState({goForTrothleUp:1,tmp});
         this.drawChartPie(this.props.legendType)
-
+        
     }
-
+    
     changelegendType(event){
         this.setCanvasSize(event.target.value);
         localStorage.setItem(this.props.wichlegend, event.target.value);
-        this.drawChartPie(event.target.value)
-
+        this.drawChartPie(event.target.value)        
+        
     }
-
+    
     async setCanvasSize(lgnd){
         const tmp=this.state.topbot;
         tmp.legendType=lgnd;
@@ -49,11 +49,11 @@ class PieChart1 extends Component {
             tmp.height=270;
         else
             tmp.height=400;
-        this.setStateAsync({topbot:tmp});
-
+        this.setStateAsync({topbot:tmp});    
+            
     }
-
-
+    
+    
     getCoordinates(legendtype,w,h){
         var offsettb=150;
         if (legendtype=="bottom"){
@@ -62,75 +62,75 @@ class PieChart1 extends Component {
             var he=h-150;
             var wi=w;
         }
-
+        
         if (legendtype=="top"){
             var startx=0;
             var starty=120
             var he=h-160;
             var wi=w;
-        }
-
+        }   
+        
         var offsetlr=150;
-
+        
         if (legendtype=="left"){
             var startx=89;
             var starty=15;
             var he=h-105;
             var wi=w;
-        }
-
+        }        
+        
         if (legendtype=="right"){
             var startx=-89;
             var starty=15;
             var he=h-105;
             var wi=w;
-        }
-
-
+        }        
+                
+        
         return {startx:startx,starty:starty,canvasheight:he,canvaswidth:wi}
     }
-
-
+    
+    
     legendTopBottom(ctx,canvasheight,data,canvaswidth,myColor){
         var w=20;
         var hightrecttext=canvasheight+42;
         for (var i = 0; i < data.length; i++) {
             if ((w)>canvaswidth){
-                w=20;
+                w=20;  
                 hightrecttext=hightrecttext+30;
             }
             ctx.fillStyle = data[i].color;
-            ctx.fillRect(w, hightrecttext-10,10,10);
+            ctx.fillRect(w, hightrecttext-10,10,10);    
 
             ctx.font = "13px Arial black";
             ctx.strokeStyle = this.state.graphFontcolor;
             ctx.fillStyle = this.state.graphFontcolor;
             ctx.fillText(data[i].name,w+15,hightrecttext);
-            w=w+170;
-        }
+            w=w+170;   
+        }           
     }
-
-    legendRightLeft(ctx,canvasheight,data,canvaswidth){
+    
+    legendRightLeft(ctx,canvasheight,data,canvaswidth){  
         var w=canvaswidth;
         var hightrecttext=canvasheight;
         for (var i = 0; i < data.length; i++) {
             ctx.fillStyle = data[i].color;
-            ctx.fillRect(w, hightrecttext-9,10,10);
+            ctx.fillRect(w, hightrecttext-9,10,10);    
 
             ctx.font = "13px Arial black";
             ctx.strokeStyle = this.state.graphFontcolor;
             ctx.fillStyle = this.state.graphFontcolor;
             ctx.fillText(data[i].name,w+15,hightrecttext);
-            hightrecttext=hightrecttext+30;
-        }
+            hightrecttext=hightrecttext+30;   
+        }           
     }
+    
 
-
-
+    
     drawChartPie(legendType){
         var canvas = document.getElementById("can");
         var ctx = this.can.current.getContext('2d');
-
+        
 
         var lastend = 0;
         var data = this.props.data; // If you add more data values make sure you add more colors
@@ -145,14 +145,14 @@ class PieChart1 extends Component {
         canvas.height = this.state.topbot.height+(150/100)*(this.state.sizepie2);
         ctx.fillStyle = this.state.graphBgcolor;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+        
         var coord=this.getCoordinates(legendType,canvas.width,canvas.height)
-
+            
         var startx=coord.startx;
-        var starty=coord.starty;
+        var starty=coord.starty;        
         var canvaswidth=coord.canvaswidth;
         var canvasheight=coord.canvasheight;
-
+        
         ctx.translate(startx, starty);
         for (var i = 0; i < data.length; i++) {
           ctx.fillStyle = data[i].color;
@@ -162,12 +162,12 @@ class PieChart1 extends Component {
           ctx.arc(canvaswidth / 2, canvasheight / 2, canvasheight / 2, lastend, lastend + (Math.PI * 2 * (data[i].value / myTotal)), false);
           ctx.lineTo(canvaswidth / 2, canvasheight / 2);
           ctx.fill();
-          lastend += Math.PI * 2 * (data[i].value / myTotal);
+          lastend += Math.PI * 2 * (data[i].value / myTotal);            
         }
-
-
-
-
+        
+        
+        
+        
         var start_angle = 0;
         for (var i = 0; i < data.length; i++){
             var val = data[i].value;
@@ -182,62 +182,62 @@ class PieChart1 extends Component {
             ctx.fillText(labelText+"%", labelX,labelY);
             start_angle += slice_angle;
         }
-
+        
         if (legendType=="bottom"){
             this.legendTopBottom(ctx,canvasheight,data,canvaswidth,myColor)
         }
-
+        
         if (legendType=="top"){
             this.legendTopBottom(ctx,-140,data,canvaswidth,myColor)
-        }
-
+        }           
+        
         if (legendType=="left"){
             this.legendRightLeft(ctx,30,data,-50,myColor)
-        }
-
+        }          
+        
         if (legendType=="right"){
             this.legendRightLeft(ctx,30,data,450,myColor)
-        }
-
-
-
-
-
+        }              
+        
+        
+        
+ 
+        
     }
-
-
-
-
-
-
-
+        
+        
+        
+        
+ 
+        
+    
 setStateAsync(state) {
     return new Promise((resolve) => {
       this.setState(state, resolve)
     });
-}
+} 
 async setImagesize(e,numb){
     await this.setStateAsync({sizepie2:numb});
     localStorage.setItem(this.props.wichgraph, numb);
     this.drawChartPie(this.state.topbot.legendType);
 }
-
-handlegraphBgcolor(color){
+ 
+handlegraphBgcolor(color){ 
    this.setState({graphBgcolor:color.hex});
    localStorage.setItem(this.props.wichgraphBgcolor, color.hex);
-   this.drawChartPie(this.state.topbot.legendType);
-}
+   this.drawChartPie(this.state.topbot.legendType);  
+}    
 
-handlegraphFontcolor(color){
+handlegraphFontcolor(color){ 
    this.setState({graphFontcolor:color.hex});
    localStorage.setItem(this.props.whichgraphFontcolor, color.hex);
    this.drawChartPie(this.state.topbot.legendType);
-}
-
+}  
+    
  render() {
     return (
-
-        <div id="pies">
+            
+        <div id="pies">     
             <div className="slided2">
                 <Slider onChangeCommitted={this.setImagesize}
                     defaultValue={this.sizepie}
@@ -245,11 +245,11 @@ handlegraphFontcolor(color){
                     aria-labelledby="discrete-slider-custom"
                     step={1}
                     valueLabelDisplay="auto"
-                    marks={this.marks}
-                />
-            </div>
+                    marks={this.marks}                        
+                /> 
+            </div> 
             <div className="clear"></div>
-
+            
             <div className="radios">
                 <div className="radiotext">Legend : bottom </div><input type="radio" className="radiobut" name="legend" value="bottom" checked={this.state.topbot.legendType === "bottom"} onChange={this.changelegendType}/>
                 <div className="radiotext">top </div><input type="radio" className="radiobut" name="legend" value="top" checked={this.state.topbot.legendType === "top"} onChange={this.changelegendType}/>
@@ -261,25 +261,25 @@ handlegraphFontcolor(color){
                 <div className="bgfont">Bg color</div>
                 <div className="bgfont1">
                 <PieChooseColor
-                    color={this.state.graphBgcolor}
-                    handleChangeElementColor={this.handlegraphBgcolor}
+                    color={this.state.graphBgcolor} 
+                    handleChangeElementColor={this.handlegraphBgcolor} 
                 />
                 </div>
                 <div className="bgfont"> Font color</div>
                 <div className="bgfont1">
                     <PieChooseColor
-                        color={this.state.graphFontcolor}
-                        handleChangeElementColor={this.handlegraphFontcolor}
-                    />
+                        color={this.state.graphFontcolor} 
+                        handleChangeElementColor={this.handlegraphFontcolor} 
+                    />  
                 </div>
             </div>
             <canvas id="can" width="500" style={{  height: "100%" }}  ref={this.can} />
             {this.state.goForTrothleUp==1 &&
-                 this.drawChartPie(this.state.topbot.legendType)
+                 this.drawChartPie(this.state.topbot.legendType)   
             }
 
       </div>
     );
   }
-}
+}    
 export default PieChart1;
--- src/index.js
+++ src/index.js
@@ -9,26 +9,37 @@ import './index.css';
 import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 const routing = (
+        
 
-
-        <div>
+        <div className="page-container">
             <Router>
-                <ul className="ul1">
-                  <li>
+                <div className="row1">                        
+                <div className="react">A React Pie Chart generator</div>
+                <ul>
+                  <li className="tmp">
                     <Link to={`${process.env.PUBLIC_URL}/`}><u>Graph1</u></Link>
                   </li>
-                  <li>
+                  <li  className="tmp">
                     <Link to={`${process.env.PUBLIC_URL}/graph2`}><u>Graph2</u></Link>
                   </li>
-                </ul>
+                </ul>                
+                </div> 
+                <div className="row centered">
                 <Switch>
                   <Route exact path={`${process.env.PUBLIC_URL}/`} component={Graph1} />
-                  <Route exact path={`${process.env.PUBLIC_URL}/graph2`} component={Graph2} />
-                </Switch>
-            </Router>
+                  <Route exact path={`${process.env.PUBLIC_URL}/graph2`} component={Graph2} />        
+                </Switch>  
+                </div>
+            </Router> 
+            <footer className="fixed-bottom">
+              <div className="footer-copyright text-center py-3">© {(new Date().getFullYear())} Copyright:
+                ydbweb.com
+              </div>
+            </footer>   
         </div>
-
+     
 
 
 )
 ReactDOM.render(routing, document.getElementById('root'))
+
